f0b5a68d129391cbe937c12015add9a8021493f3
aasunun888 <118214338+aasunun888@users.noreply.github.com>
2022-11-22 21:14:16 +0100

    cambio hello por wola
    se ha modificado...

--- ProyectoGitRamasEjemplo/src/HOLAMUNDO.java
+++ ProyectoGitRamasEjemplo/src/HOLAMUNDO.java
@@ -3,7 +3,7 @@ public class HOLAMUNDO {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello mundo");
+		System.out.println("wola mundo");
 	}
 	public static void imprimeSaludo() { 
 System.out.println(“Hei”);	
